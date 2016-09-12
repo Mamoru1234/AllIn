5b2f54ea64f52e5560966b4e48e0d9456b892a7a
Alexei <f.gontar@yandex.ua>
2016-09-12 16:01:47 +0300

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt-get install --yes python-software-properties
+add-apt-repository ppa:webupd8team/java
+apt-get update -qq
+echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections
+apt-get install --yes oracle-java8-installer
+yes "" | apt-get -f install
+
+# start server
