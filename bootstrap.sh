3f1976ce83c03fa215842f9aa51902f64fbb8fbe
Alexei <f.gontar@yandex.ua>
2016-09-12 19:13:06 +0300

    MySQL with simple spring server

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,4 +8,12 @@ echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-se
 apt-get install --yes oracle-java8-installer
 yes "" | apt-get -f install
 
+apt-get update
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+apt-get update
+apt-get -y install mysql-server
+
 # start server
+cd /vagrant
+./gradlew :server:run
--- server/src/main/java/com/github/mamoru/allin/server/Server.java
+++ server/src/main/java/com/github/mamoru/allin/server/Server.java
@@ -1,10 +1,25 @@
 package com.github.mamoru.allin.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by alexei on 12.09.16.
  */
+@Controller
+@EnableAutoConfiguration
 public class Server {
-    public static void main(String... args) {
-        System.out.println("hello");
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Server.class, args);
     }
 }
