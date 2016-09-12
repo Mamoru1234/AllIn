c995c15e524349ea31d9d9bcf5bacf2c5de44e96
Alexei <f.gontar@yandex.ua>
2016-09-12 16:50:35 +0300

    Basic gradle structure

---
+++ server/src/main/java/com/github/mamoru/allin/server/Server.java
@@ -0,0 +1,10 @@
+package com.github.mamoru.allin.server;
+
+/**
+ * Created by alexei on 12.09.16.
+ */
+public class Server {
+    public static void main(String... args) {
+        System.out.println("hello");
+    }
+}
